f4f76ec3ca9a1839ecaae92e7fccec1938cc5b2f
Richard Livsey <richard@livsey.org>
2008-09-07 20:41:57 +0100

    making thread safe

--- lib/dm-userstamp.rb
+++ lib/dm-userstamp.rb
@@ -4,7 +4,29 @@ gem 'dm-core', '=0.9.4'
 require 'dm-core'
 
 module DataMapper
+  
   module Userstamp
+    
+    module Stamper
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+      
+      module ClassMethods
+        
+        def current_user=(user)
+          Thread.current["#{self.to_s.downcase}_#{self.object_id}_stamper"] = user
+        end
+        
+        def current_user
+          Thread.current["#{self.to_s.downcase}_#{self.object_id}_stamper"]
+        end
+        
+      end
+      
+    end
+    
+    
     USERSTAMP_PROPERTIES = {
       :created_by_id => lambda { |r| r.created_by_id = User.current_user.id if User.current_user && r.new_record? && r.created_by_id.nil? },
       :updated_by_id => lambda { |r| r.updated_by_id = User.current_user.id if User.current_user}
